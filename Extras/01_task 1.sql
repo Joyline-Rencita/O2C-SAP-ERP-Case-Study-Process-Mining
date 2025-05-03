aaa64702b02af1a1aa26e35a99ab7d14528ff106
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-03 19:31:57 +0400

    Create 01_task 1.sql

---
+++ Extras/01_task 1.sql
@@ -0,0 +1,3 @@
+Return on Investment or ROI
+
+The correct calculation is:$1B * 10% (early payments) * 90% (early payments resolved by actions) * 0.02% (Daily RoIC) * 10 (average # days early) = $180,000$180,000 - $75,000 (cost of implementing improvements) = $105,000
