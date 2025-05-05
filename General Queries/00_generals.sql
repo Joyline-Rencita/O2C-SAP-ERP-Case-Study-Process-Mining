4372b3684dbfa605ca1a2bb9d3d5cb133ebcfc10
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-05 13:28:05 +0400

    Update 00_generals.sql

--- General Queries/00_generals.sql
+++ General Queries/00_generals.sql
@@ -10,3 +10,16 @@ AVG(CASE WHEN "EVENTLOG"."USERTYPE" = 'Batch' THEN 1.0 ELSE 0.0 END)
 Example of a Connection KPI for Duration:
 
 AVG(1.0*DATEDIFF(dd, SOURCE("EVENTLOG"."EVENTTIME"), TARGET("EVENTLOG"."EVENTTIME")))
+
+
+3. Process Query Language (PQL) - Short Overview
+
+| **Syntax**                                                   | **Example**                                     | **Description**                                                            |
+| ------------------------------------------------------------ | ----------------------------------------------- | -------------------------------------------------------------------------- |
+| `FILTER PROCESS <STATEMENT> <activity>`                      | `FILTER PROCESS EQUALS 'Create Purchase Order'` | Filters for all cases that include the activity "Create Purchase Order".   |
+| `FILTER <FUNCTION>("TABLE_NAME"."COLUMN_NAME") <op> <value>` | `FILTER YEAR("EVENTLOG"."EVENTTIME") = 2010`    | Filters for all occurrences in the year 2010.                              |
+| `FILTER "TABLE_NAME"."COLUMN_NAME" <op> <value>`             | `FILTER "EVENTLOG"."USER_TYPE" != 'BATCH'`      | Filters for non-automated actions in the process (i.e., excludes 'BATCH'). |
+
+
+4. 
+
