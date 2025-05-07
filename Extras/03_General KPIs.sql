8d7548473d792812a1327ad05f0f1a05bb697895
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-07 10:03:39 +0400

    Update 03_General KPIs.sql

--- Extras/03_General KPIs.sql
+++ Extras/03_General KPIs.sql
@@ -34,5 +34,42 @@ NOT IN ( 33. VARIABLE_AUTO_TYPES ) ) = 0 THEN 1.0 ELSE 0.0 END)*100
 Formula: AVG(CALC_THROUGHPUT(CASE_START TO 
 CASE_END, REMAP_TIMESTAMPS("_CEL_O2C_ACTIVITIES"."EVENTTIME", HOURS))/24)
 
+6. 
 
+*******************************************************************************************************************************************************************
 
+                                                  Variables Name and Value
+
+  
+Variable name: VARIABLE_ACTIVITY_COLUMN  
+Variable value: "ACTIVITY_EN"
+
+Variable name: VARIABLE_UNDESIRED_ACTIVITIES  
+Variable value: 'Change Billing Block','Change Confirmed Delivery Date','Change Confirmed Goods Issue Date','Change Confirmed Quantity','Change Delivery Block','Change Freight Terms','Change Inco Terms (Part 1)','Change Inco Terms (Part 2)','Change Item Credit Price','Change Material','Change Material Availability Date','Change Payment Terms','Change Plant','Change Price','Change Reason for Rejection','Change Requested Delivery Date','Change Requested Goods Issue Date','Change Requested Quantity','Change Route','Change Shipping Type','Change Storage Location','Set Delivery Block','Remove Delivery Block','Set Credit Block','Remove Credit Block','Set Billing Block','Remove Billing Block','Create Returns delivery for order','Set Reason for Rejection'
+
+Variable name: VARIABLE_REJECTION_ACTIVITIES  
+Variable value: 'Set Reason for Rejection'
+
+Variable name: VARIABLE_RECORD_GOODS_ISSUE_ACTIVITY  
+Variable value: 'Record Goods Issue'
+
+Variable name: VARIABLE_DUE_DATE  
+Variable value: PU_LAST("VBAP"."VBEP"."TS_EDATU","VBEP"."BMENG" > 0)
+
+Variable name: VARIABLE_TOO_LATE_BUFFER  
+Variable value: round( parseNumber( 0 , , ) )
+
+Variable name: VARIABLE_TOO_EARLY_BUFFER  
+Variable value: round( parseNumber( 3 , , ) )
+
+Variable name: VARIABLE_ROUTE  
+Variable value: "VBAP"."ROUTE_IN_DAYS"
+
+Variable name: VARIABLE_AUTO_TYPES  
+Variable value: 'B','S'
+
+Variable name: VARIABLE_CREATE_INVOICE_ACTIVITY  
+Variable value: 'Create Invoice','Create Pro forma invoice'
+
+Variable name: CREATION_DATE  
+Variable value: "VBAK"."TS_ERDAT"
