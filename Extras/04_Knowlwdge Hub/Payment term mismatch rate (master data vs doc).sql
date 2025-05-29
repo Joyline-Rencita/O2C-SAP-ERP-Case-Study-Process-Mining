c373f48729251d0adbba599763a988b387c4b42f
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-29 11:22:22 +0530

    Create Payment term mismatch rate (master data vs doc).sql

---
+++ Extras/04_Knowlwdge Hub/Payment term mismatch rate (master data vs doc).sql
@@ -0,0 +1,7 @@
+Pseudo-code
+Count of Payment Terms Mismatches (Unfavorable)
+/
+Distinct Count of Cleared Documents
+
+KPI("AP_Metric_VendorAccountCreditItem_COUNT__PaymentTermsMismatchVmdVsDocumentUnfavorable")
+/ KPI("AP_Metric_VendorAccountCreditItem_COUNTDISTINCT__NumberOfClearedDocuments")
