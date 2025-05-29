032234bee23f5f6766b62452098cb2eb264b6d51
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-29 11:19:57 +0530

    Create on-time pyment.sql

---
+++ Extras/04_Knowlwdge Hub/on-time pyment.sql
@@ -0,0 +1,7 @@
+KPI("AP_Metric_VendorAccountCreditItem_SUM__OnTimePaymentByDocumentValue")
+/ KPI("AP_Metric_VendorAccountCreditItem_SUM__NetClearedDocumentValueConverted")
+
+Pseudo-code
+Sum of On-time Payments by Document Value
+/
+Sum of Net Cleared Document Values Converted
