4bdf0defcb6a60c791ada37e508d414466f588a9
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-30 19:56:56 +0530

    Create Manual clearing rate.sql

---
+++ Extras/04_Knowlwdge Hub/Manual clearing rate.sql
@@ -0,0 +1,8 @@
+Pseudo-code
+Count of Documents with Manual Clearing
+/
+Distinct Count of Cleared Documents
+
+KPI("AP_Metric_VendorAccountCreditItem_COUNT__DocumentHasManualClearing")
+/
+KPI("AP_Metric_VendorAccountCreditItem_COUNTDISTINCT__NumberOfClearedDocuments")
