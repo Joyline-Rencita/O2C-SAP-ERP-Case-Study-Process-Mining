e7d84057d774bec9a2f092e0e367f47efa17c53c
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-05-30 19:54:43 +0530

    Update Distinct doc. payment terms.sql

--- Extras/04_Knowlwdge Hub/Distinct doc. payment terms.sql
+++ Extras/04_Knowlwdge Hub/Distinct doc. payment terms.sql
@@ -1,3 +1,7 @@
+Pseudo-code
+Count of Distinct Non-NULL Payment Terms
+for Documents that are not Open and Cleared
+
 COUNT(DISTINCT
   CASE
     WHEN "o_celonis_VendorAccountCreditItem"."isRelevant" = 0
