9c3df7bbdc8a91fc6e9973b9981049ae91426e52
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-10 17:08:39 +0530

    Update Free Text order Count.sql

--- Extras/04_Knowlwdge Hub/Free Text order Count.sql
+++ Extras/04_Knowlwdge Hub/Free Text order Count.sql
@@ -1,1 +1,5 @@
+Description
+Number of purchase order items that have a free text line item(s) rather than choosing from a item master or catalog.
+Unit None
+
 SUM("o_celonis_PurchaseOrderItem"."FreeTextOrder")
