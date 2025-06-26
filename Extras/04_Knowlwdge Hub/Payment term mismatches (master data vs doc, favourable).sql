04a9477ca16dbe594a498c7c63f55f725b27c03f
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 10:39:24 +0530

    Create Payment term mismatches (master data vs doc, favourable).sql

---
+++ Extras/04_Knowlwdge Hub/Payment term mismatches (master data vs doc, favourable).sql
@@ -0,0 +1,17 @@
+Description
+Number of document items affected by favorable mismatches between document and vendor master data
+
+Pseudo-code
+Sum of Indicator for Documents:
+    If Generic Validation for Open and Cleared Documents is 0, then 0.0
+    Else If Payment Terms Mismatch is 'Favorable Mismatch', then 1.0
+    Else 0.0
+
+
+COUNT(
+  CASE
+    WHEN "o_celonis_VendorAccountCreditItem"."isRelevant"=1
+      AND "o_celonis_VendorAccountCreditItem"."PaymentTermsMismatchVmdVsDocument"='Favorable Mismatch'
+      THEN 1
+    ELSE NULL
+  END)
