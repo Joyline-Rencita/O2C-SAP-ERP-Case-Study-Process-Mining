16912b21afd687c8d21b9955821a44a55be32bb3
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 10:31:12 +0530

    Create Paid early (% by Doc. value).sql

---
+++ Extras/04_Knowlwdge Hub/Paid early (% by Doc. value).sql
@@ -0,0 +1,8 @@
+Pseudo-code
+Sum of Early Payment Amounts for Documents
+/
+Sum of Net Cleared Document Values (Converted)
+
+
+KPI("AP_Metric_VendorAccountCreditItem_SUM__EarlyPaymentByDocumentValue")
+      / KPI("AP_Metric_VendorAccountCreditItem_SUM__NetClearedDocumentValueConverted")
