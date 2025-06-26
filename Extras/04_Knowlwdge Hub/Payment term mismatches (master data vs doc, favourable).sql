1dc7e060195ce416e7fa40bbbe5ed5d6ceb23bd4
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-06-26 10:43:20 +0530

    Update Payment term mismatches (master data vs doc, favourable).sql

--- Extras/04_Knowlwdge Hub/Payment term mismatches (master data vs doc, favourable).sql
+++ Extras/04_Knowlwdge Hub/Payment term mismatches (master data vs doc, favourable).sql
@@ -1,12 +1,11 @@
 Description
 Number of document items affected by favorable mismatches between document and vendor master data
 
-Pseudo-code
+Pseudo-code :
 Sum of Indicator for Documents:
     If Generic Validation for Open and Cleared Documents is 0, then 0.0
     Else If Payment Terms Mismatch is 'Favorable Mismatch', then 1.0
     Else 0.0
-
 
 COUNT(
   CASE
