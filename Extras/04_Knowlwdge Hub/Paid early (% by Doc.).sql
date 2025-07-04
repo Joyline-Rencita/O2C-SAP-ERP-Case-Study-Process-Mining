730e5c5c12249e8e7fd608ec4babcd0e5a4a6836
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-04 10:29:39 +0530

    Create Paid early (% by Doc.).sql

---
+++ Extras/04_Knowlwdge Hub/Paid early (% by Doc.).sql
@@ -0,0 +1,10 @@
+Description :
+Percentage of documents value paid before the due date taking any grace period into account. For documents with a cash discount option, the cash discount due date is used; otherwise, the net due date is used.
+
+Pseudo-code
+Count of Early Payments by Document
+/
+Count of Distinct Cleared Documents
+
+KPI("AP_Metric_VendorAccountCreditItem_COUNT__EarlyPaymentByDocument")
+ / KPI("AP_Metric_VendorAccountCreditItem_COUNTDISTINCT__NumberOfClearedDocuments")
