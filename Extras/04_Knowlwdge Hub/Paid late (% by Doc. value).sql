5433094b7aeacbe3d3ad0692dae44ec0b183c4e9
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-04 10:30:35 +0530

    Create Paid late (% by Doc. value).sql

---
+++ Extras/04_Knowlwdge Hub/Paid late (% by Doc. value).sql
@@ -0,0 +1,10 @@
+Description
+Percentage of document value paid after the due date taking any grace period into account. For documents with a cash discount option, the cash discount due date is used; otherwise, the net due date is used.
+
+Pseudo-code
+Sum of Late Payments by Document Value
+/
+Sum of Net Cleared Document Values Converted
+
+KPI("AP_Metric_VendorAccountCreditItem_SUM__LatePaymentByDocumentValue")
+      / KPI("AP_Metric_VendorAccountCreditItem_SUM__NetClearedDocumentValueConverted")
