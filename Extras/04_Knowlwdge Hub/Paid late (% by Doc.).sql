6a3197e2319e32833b55c6437edf3c976d66138d
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-04 10:37:15 +0530

    Update Paid late (% by Doc.).sql

--- Extras/04_Knowlwdge Hub/Paid late (% by Doc.).sql
+++ Extras/04_Knowlwdge Hub/Paid late (% by Doc.).sql
@@ -1,10 +1,11 @@
-Description
+Description :
 Percentage of documents paid after the due date taking any grace period into account. For documents with a cash discount option, the cash discount due date is used; otherwise, the net due date is used.
 
-Pseudo-code
+Pseudo-code :
 Count of Late Payments by Document
 /
 Distinct Count of Cleared Documents
 
+  Code :
 KPI("AP_Metric_VendorAccountCreditItem_COUNT__LatePaymentByDocument")
 /  KPI("AP_Metric_VendorAccountCreditItem_COUNTDISTINCT__NumberOfClearedDocuments")
