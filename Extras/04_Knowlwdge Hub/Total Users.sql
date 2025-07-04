d97e7e7d7e068e56546e75bf296ed4be47a8d6cc
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-04 10:42:16 +0530

    Create Total Users.sql

---
+++ Extras/04_Knowlwdge Hub/Total Users.sql
@@ -0,0 +1,8 @@
+Description : 
+Total number of distinct users
+
+Unit None  ;   Format ,.0f;      Desired direction NONE;
+
+Code :
+
+COUNT (DISTINCT ${UNIVERSAL_Variable_INPUT__USERNAME_RUNTIME})
