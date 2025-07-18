564628686af5bc47e090436d0c3d0be3fc95f98e
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-18 10:21:09 +0530

    Delete Extras/04_Knowlwdge Hub/Days of Cover (12M).sql

--- Extras/04_Knowlwdge Hub/Days of Cover (12M).sql
+++
@@ -1,9 +0,0 @@
-365 * KPI(IM_Metric_MaterialMasterPlant_SUM__InventoryOnHandValue) /
-(
-  KPI(IM_Metric_MaterialMasterPlant_SUM__ConsumptionAmount12Months)
-  +
-  CASE
-    WHEN ${IM_Variable_INPUT__DaysOfCoverDueDate} LIKE 'Consider'
-    THEN  KPI(IM_Metric_MaterialMasterPlant_SUM__OpenSalesOrderItemBackorderAmount)
-END
-)
