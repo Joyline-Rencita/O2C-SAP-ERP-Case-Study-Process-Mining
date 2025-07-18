ab893a51a1741b87ba4a0412c674f3c1540b6511
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-18 10:24:12 +0530

    Delete Extras/04_Knowlwdge Hub/Manual Activities.sql

--- Extras/04_Knowlwdge Hub/Manual Activities.sql
+++
@@ -1,6 +0,0 @@
-Description
-Count the selected automated activities NOT executed by selected automated user types
-Unit None
-
-sum( case when ${UNIVERSAL_Variable_INPUT__USERTYPE_RUNTIME} not in (${UNIVERSAL_Variable_INPUT__AUTOMATEDUSERS_RUNTIME})
- then 1.0 else 0.0 end)
