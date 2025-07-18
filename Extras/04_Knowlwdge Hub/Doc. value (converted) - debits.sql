ea003cbbf855abe97e37a97918c3dc8b7c1673bb
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-18 10:21:38 +0530

    Delete Extras/04_Knowlwdge Hub/Doc. value (converted) - debits.sql

--- Extras/04_Knowlwdge Hub/Doc. value (converted) - debits.sql
+++
@@ -1,9 +0,0 @@
-Pseudo-code
-Sum of Invoice Values for Open and Cleared Documents
-
-
-SUM(
-    CASE WHEN  "o_celonis_VendorAccountDebitItem"."isRelevant" = 1
-    THEN "o_celonis_VendorAccountDebitItem"."ConvertedDocumentValue"
-    ELSE 0.0 END
-  )
