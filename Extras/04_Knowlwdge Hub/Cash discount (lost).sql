9ff92de5db4d1cb384ea43c22b32fe0fe265f22e
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-18 10:19:43 +0530

    Delete Extras/04_Knowlwdge Hub/Cash discount (lost).sql

--- Extras/04_Knowlwdge Hub/Cash discount (lost).sql
+++
@@ -1,9 +0,0 @@
-Pseudo-code
-Sum of Lost Cash Discount for valid documents
-
-SUM(
-  CASE
-    WHEN "o_celonis_VendorAccountCreditItem"."isRelevantAndCleared" = 0.0
-      THEN 0.0
-    ELSE "o_celonis_VendorAccountCreditItem"."LostCashDiscount"
-  END)
