880b801ac3b353c83dffd444c1cb6c8a60ab9d0a
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-19 22:05:25 +0530

    Delete Extras/04_Knowlwdge Hub/Turnover Rate (12M).sql

--- Extras/04_Knowlwdge Hub/Turnover Rate (12M).sql
+++
@@ -1,7 +0,0 @@
-CASE
-  WHEN ${IM_Variable_INPUT__TurnoverRateConsideredInput} LIKE 'Finished Goods Only'
-  THEN
-    KPI(IM_Metric_MaterialMasterPlant_SUM__ConsumptionAmountFinishedGoods12Months) / KPI(IM_Metric_MaterialMasterPlant_SUM__AvgInventoryAmountFinishedGoods12Months)
-  ELSE
-    KPI(IM_Metric_MaterialMasterPlant_SUM__ConsumptionAmount12Months) / KPI(IM_Metric_MaterialMasterPlant_SUM__AvgInventoryAmount12Months)
-END
