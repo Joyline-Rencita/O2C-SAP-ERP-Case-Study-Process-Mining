9cc6b9c818c4a034b2633bacdf687b4d778931d8
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-19 20:40:12 +0530

    Delete Extras/04_Knowlwdge Hub/Process Variants.sql

--- Extras/04_Knowlwdge Hub/Process Variants.sql
+++
@@ -1,9 +0,0 @@
-Pseudo-code
-Count of Distinct Shortened Activities for Valid Documents
-
-COUNT(DISTINCT
-  CASE
-    WHEN "o_celonis_VendorAccountCreditItem"."isRelevant" = 0
-      THEN NULL
-    ELSE SHORTENED ( VARIANT ( "el__VendorAccountCreditItemActivities"."ACTIVITY" ) )
-  END)
