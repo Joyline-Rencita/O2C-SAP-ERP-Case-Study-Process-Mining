d6ccef190af9d8842e8d6cb55934f3404e0a16b9
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-19 20:39:37 +0530

    Delete Extras/04_Knowlwdge Hub/Payment term mismatch rate (master data vs doc, unfavourable).sql

--- Extras/04_Knowlwdge Hub/Payment term mismatch rate (master data vs doc, unfavourable).sql
+++
@@ -1,7 +0,0 @@
-Pseudo-code
-Count of Payment Terms Mismatches (Unfavorable)
-/
-Distinct Count of Cleared Documents
-
-KPI("AP_Metric_VendorAccountCreditItem_COUNT__PaymentTermsMismatchVmdVsDocumentUnfavorable")
-/ KPI("AP_Metric_VendorAccountCreditItem_COUNTDISTINCT__NumberOfClearedDocuments")
