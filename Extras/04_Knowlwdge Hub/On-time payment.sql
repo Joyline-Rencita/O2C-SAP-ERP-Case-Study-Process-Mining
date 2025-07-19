bad78c16b0f34896ad7207f708d458975e7d3ba5
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-07-19 20:36:54 +0530

    Delete Extras/04_Knowlwdge Hub/On-time payment.sql

--- Extras/04_Knowlwdge Hub/On-time payment.sql
+++
@@ -1,7 +0,0 @@
-KPI("AP_Metric_VendorAccountCreditItem_SUM__OnTimePaymentByDocumentValue")
-/ KPI("AP_Metric_VendorAccountCreditItem_SUM__NetClearedDocumentValueConverted")
-
-Pseudo-code
-Sum of On-time Payments by Document Value
-/
-Sum of Net Cleared Document Values Converted
